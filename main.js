b7ebd458d76fd1febe6d88643a301b6ad1de799f
Tristan Schmitt <trschmitt50@live.com>
2017-07-18 15:16:12 -0400

    first fetch result working

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
+let list = document.querySelector('#list');
+
+`<button onclick="myFunction()">Search</button>`
+function searchFun() {
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "search");
+    document.body.appendChild(x);
+}
+
+
+
 fetch("https://crossorigin.me/http://www.recipepuppy.com/api")
 .then(
   function(response) {
 
     if (response.status !== 200) {
-      console.log("The status is "response.status);
+      console.log("The status is " + response.status);
     }
 
     response.json().then(function(recipeData) {
       let results = recipeData.results;
       results.forEach(function(recipe) {
         let template = `
-        <ul>
-          <li>
-            <img src=${recipe.thumbnail}>
-            <a href=${recipe.href}><h2>${recipe.title}</h2></a>
-
-          </li>
-        </ul>
+        <li>
+          <img src=${recipe.thumbnail} alt="recipe-picture-not-found">
+          <a href=${recipe.href}><h4>${recipe.title}</h4></a>
+        </li>
         `
-
-      })
-
-    })
-
+        list.innerHTML = template;
+      });
+    });
   }
-
 )
